94daa7ac2cf3084f46dad750ccb3083351d5b4fe
XiaohanLi594 <2601991635@qq.com>
2025-04-12 09:46:39 +0800

    creat menu UI
    creat pause menu UI

--- Assets/Team/Scripts/ButtonSound.cs
+++ Assets/Team/Scripts/ButtonSound.cs
@@ -3,18 +3,16 @@ using UnityEngine.UI;
 
 public class ButtonSound : MonoBehaviour
 {
-    public AudioSource audioSource; // 引用音频源
+    public AudioSource audioSource;
 
     void Start()
     {
-        // 获取按钮组件并添加点击事件
         Button button = GetComponent<Button>();
         button.onClick.AddListener(OnButtonClick);
     }
 
     void OnButtonClick()
     {
-        // 播放音效
         if (audioSource != null)
         {
             audioSource.Play();
