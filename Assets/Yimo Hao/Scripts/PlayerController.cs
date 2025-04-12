804fd7627b1983b769a9a06774df9e06ee806732
Haoyimo <1187908404@qq.com>
2025-04-12 22:06:40 +0800

    a little changes

--- Assets/Yimo Hao/Scripts/PlayerController.cs
+++ Assets/Yimo Hao/Scripts/PlayerController.cs
@@ -1,55 +1,37 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
- // Rigidbody of the player.
- private Rigidbody rb; 
+    private Rigidbody rb;
+    private Vector2 input;
+    public float speed = 5f;
 
- // Movement along X and Y axes.
- private float movementX;
- private float movementY;
-
- // Speed at which the player moves.
- public float speed = 0; 
-
- // Start is called before the first frame update.
- void Start()
+    void Start()
     {
- // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
-    }
- 
- // This function is called when a move input is detected.
- void OnMove(InputValue movementValue)
-    {
- // Convert the input value into a Vector2 for movement.
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
- // Store the X and Y components of the movement.
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
+        rb.freezeRotation = true; // 冻结物理旋转
     }
 
- // FixedUpdate is called once per fixed frame-rate frame.
- private void FixedUpdate() 
+    void OnMove(InputValue value)
     {
- // Create a 3D movement vector using the X and Y inputs.
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-
- // Apply force to the Rigidbody to move the player.
-        rb.AddForce(movement * speed); 
+        input = value.Get<Vector2>();
+        Debug.Log($"Input: ({input.x}, {input.y})");
     }
 
- void OnTriggerEnter(Collider other) 
+    private void FixedUpdate()
     {
- // Check if the object the player collided with has the "PickUp" tag.
- if (other.gameObject.CompareTag("PickUp")) 
+        // 使用速度控制+输入平滑
+        Vector3 targetVelocity = new Vector3(input.x, 0, input.y) * speed;
+        rb.velocity = Vector3.Lerp(rb.velocity, targetVelocity, 0.5f);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
         {
- // Deactivate the collided object (making it disappear).
             other.gameObject.SetActive(false);
         }
+
     }
-
-
 }
