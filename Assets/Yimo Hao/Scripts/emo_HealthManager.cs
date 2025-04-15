042257bc9b115b44979788b69142184477ab9ee9
Haoyimo <1187908404@qq.com>
2025-04-15 16:37:37 +0800

    a little changes

---
+++ Assets/Yimo Hao/Scripts/emo_HealthManager.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+
+public class emo_HealthManager : MonoBehaviour
+{
+    public static emo_HealthManager Instance;
+
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    public TMP_Text healthText;
+    public TMP_Text otherPanelHealthText;
+    public GameObject currentPanel;
+    public GameObject gameOverPanel;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        if (healthText != null)
+        {
+            healthText.text = ": " + currentHealth;
+        }
+
+
+        if (otherPanelHealthText != null)
+        {
+            otherPanelHealthText.text = "" + currentHealth;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            GameOver();
+        }
+        UpdateHealthUI();
+    }
+
+    private void GameOver()
+    {
+        currentPanel.SetActive(false);
+        gameOverPanel.SetActive(true);
+    }
+}
+
+
+
+
+
+
+
+
